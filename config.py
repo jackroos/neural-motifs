13d0f4ed23b86a8b50bc939df7f28dbe83b30daa
jackroos237 <jackroos237@gmail.com>
2019-02-19 20:46:26 +0800

    update path.

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ def stanford_path(fn):
 # =============================================================================
 # Update these with where your data is stored ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-VG_IMAGES = '/home/rowan/datasets2/VG_100K_2/VG_100K'
+# VG_IMAGES = '/home/rowan/datasets2/VG_100K_2/VG_100K'
+VG_IMAGES = os.path.join(DATA_PATH, 'visual_genome', 'VG_100K')
 RCNN_CHECKPOINT_FN = path('faster_rcnn_500k.h5')
 
 IM_DATA_FN = stanford_path('image_data.json')
